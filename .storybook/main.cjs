58ca3682febe22bb02c84100b650ab9f9695bd93
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-10-12 22:12:35 -0300

    ci: fix

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -12,12 +12,5 @@ module.exports = {
   features: {
     storyStoreV7: true,
   },
-  // viteFinal: (config, { configType }) => {
-  //   if (configType === "PRODUCTION") {
-  //     config.base = "/ignite-lab-design-system/";
-  //   }
-
-  //   return config;
-  // },
 };
 
